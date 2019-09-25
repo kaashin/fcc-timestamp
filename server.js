0733f5bab0efac30112e0e086dd37c73160d9831
Richard Lee <richard@kaashin.com>
2019-09-24 22:45:39 -0600

    first commit with my solution

---
+++ server.js
@@ -0,0 +1,67 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/timestamp", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const dateString = req.params.date_string;
+  let unixDate, utcDate, validDate, date;
+
+  //   const inputType = dateString === parseInt(dateString).toString() ? 'unix' : 'other';
+
+  // Check if it is a unix date
+  if (dateString === parseInt(dateString).toString()) {
+    console.log("input was unix");
+    date = new Date(parseInt(dateString));
+    console.log(date);
+    unixDate = dateString;
+    utcDate = date.toUTCString();
+  } else {
+    console.log("input was a string");
+    date = new Date(dateString);
+    if (date != "Invalid Date") {
+      unixDate = date.getTime();
+      utcDate = date.toUTCString();
+    } else {
+      unixDate = null;
+      utcDate = "Invalid Date";
+    }
+  }
+
+  res.json({
+    unix: unixDate,
+    utc: utcDate
+  });
+});
+
+// listen for requests :)
+var listener = app.listen("8080", function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
